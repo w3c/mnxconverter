e57fefb3d6c1b25bfd4e43bb4d995b526d68d6b1
Adrian Holovaty <adrian@holovaty.com>
2023-11-09 12:32:22 +0100

    Made error display nicer for convert.py.
    Previously, in case of an import error, we displayed the entire
    Python traceback. Now we just display the specific error message.

--- convert.py
+++ convert.py
@@ -1,8 +1,12 @@
-from mnxconverter.musicxml import get_score as get_score_from_musicxml
+from mnxconverter.musicxml import NotationDataError, NotationImportError, get_score as get_score_from_musicxml
 from mnxconverter.mnx import put_score as put_mnx_score
 import sys
 
 if __name__ == "__main__":
     filedata = open(sys.argv[1], 'rb').read()
-    s = get_score_from_musicxml(filedata)
-    print(put_mnx_score(s).decode('utf8'))
+    try:
+        s = get_score_from_musicxml(filedata)
+    except (NotationDataError, NotationImportError) as e:
+        print(f'Error: {e.args[0]}')
+    else:
+        print(put_mnx_score(s).decode('utf8'))
